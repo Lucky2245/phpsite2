380a7f7fae4c9d01a7bce1184513d5036499b23a
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-10-03 18:31:59 -0400

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,1 +1,12 @@
 <?php echo $_POST["username"];?>
+<?php 
+    echo "This page will save your username.";
+    setcookie("test_cookie", "test", time() + 3600, '/');
+?>
+<?php 
+  if(count($_COOKIE) > 0){
+    //set the cookie
+  }else{
+    echo "We can't save your username if cookies aren't enabled!";
+  }
+?>
